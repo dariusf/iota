789d04f82525723e9c3c95af89d451993dc5c743
Darius Foo <darius.foo.tw@gmail.com>
2014-05-17 19:18:45 +0800

    Restored CLI functionality

--- iota.js
+++ iota.js
@@ -11,16 +11,25 @@ var runtimeLibCode = fs.readFileSync(__dirname + '/src/lib.js', {encoding: 'utf-
 var boilerplateBefore = fs.readFileSync(__dirname + '/src/boilerplate/before.js', {encoding: 'utf-8'});
 var boilerplateAfter = fs.readFileSync(__dirname + '/src/boilerplate/after.js', {encoding: 'utf-8'});
 
+function compileCode(input, includeLibrary, boilerplate) {
+	var result = compile.compile(input);
+
+	result = (boilerplate ? boilerplateBefore : "") + result + (boilerplate ? boilerplateAfter : "");
+	result = includeLibrary ? (runtimeLibCode + result) : result;
+
+	return result.trim();
+}
+
+function parse(input) {
+	return compile.parse(input);
+}
+
+if (filename) {
+	var code = fs.readFileSync(path.resolve(__dirname, filename), {encoding: 'utf-8'});
+	console.log(JSON.stringify(parse(code)));
+}
+
 module.exports = {
-	compile: function (input, includeLibrary, boilerplate) {
-		var result = compile.compile(input);
-
-		result = (boilerplate ? boilerplateBefore : "") + result + (boilerplate ? boilerplateAfter : "");
-		result = includeLibrary ? (runtimeLibCode + result) : result;
-
-		return result.trim();
-	},
-	parse: function (input) {
-		return compile.parse(input);
-	}
+	compile: compileCode,
+	parse: parse
 };
