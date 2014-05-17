9a0725323254a7f9db73c6effe101c991179f79c
Darius Foo <darius.foo.tw@gmail.com>
2014-05-17 18:27:41 +0800

    Cleanup

--- src/pratt.js
+++ src/pratt.js
@@ -1,35 +1,8 @@
 
 // AST nodes
 
-// function Expression (e) {
-// 	return {
-// 		type: "expression",
-// 		e: e
-// 	};
-// }
-
-// function Application (op) {
-// 	var args = Array.prototype.slice.call(arguments, 1);
-// 	return {
-// 		type: "application",
-// 		op: op,
-// 		operands: args
-// 	};
-// }
-
-function Message (value) {
-	return {
-		type: 'message',
-		value: value
-	};
-}
-
 function Chain (left, op, right) {
-	// var args = Array.prototype.slice.call(arguments);
 	var result = [];
-	// args.forEach(function(message) {
-	// 	result.value.push(message);
-	// });
 	result.push(left);
 	result.push(op);
 	op.value.arguments.push({type: 'chain', value: right});
@@ -38,34 +11,8 @@ function Chain (left, op, right) {
 
 // Tokens
 
-// function Id (i) {
-// 	return {type: "identifier", value: i};
-// }
-// function Operator (i) {
-// 	return {type: "operator", value: i};
-// }
-var input = [];//[Id('a'), Operator('+'), Id('b')];
+var input = [];
 var ptr = 0;
-
-// function lex (str) {
-// 	var allowed = /\b[0-9a-zA-Z]+\b|[-+/()~!*?:]/g;
-// 	var ops = /[-+/()~!*?:]/;
-
-// 	var match;
-// 	var results = [];
-
-// 	while (match = allowed.exec(str)) {
-// 		var text = match[0];
-// 		if (ops.test(text)) {
-// 			results.push(Operator(text));
-// 		} else {
-// 			results.push(Id(text));
-// 		}
-// 	}
-// 	results.push({type: 'EOF'});
-
-// 	return results;
-// }
 
 function consume () {
 	return input[ptr++];
@@ -133,7 +80,6 @@ var otherOperators = {
 	"==": makeInfixOp(COMPARISON),
 	"and": makeInfixOp(BOOLEAN),
 	// "/": makeInfixOp(PRODUCT),
-//brackets
 	// "?": parseMixfixOp
 };
 
@@ -185,25 +131,9 @@ function parseExpression (precedence) {
 		consume();
 
 		left = infixParser(left, token);
-		// console.log('left');
-		// print(left);
 	}
 
 	return left;
-}
-
-// entry point
-
-// var str = "+-!a";
-// var str = "a + b * c";
-// var str = "a * b + c";
-// input = lex(str);
-// console.log(input);
-// console.log(JSON.stringify(parseExpression(0)));
-
-var util = require('util');
-function print (obj) {
-	console.log(util.inspect(obj, {showHidden: false, depth: null}));
 }
 
 module.exports = {
@@ -215,11 +145,8 @@ module.exports = {
 		input = chain.value.slice();
 		input.push({type: 'EOF'});
 		var result = parseExpression(0);
-		// console.log("=>");
-		// print(result);
 
 		return result;
-		// return {type: 'chain', value: [result]};
 	},
 	isOperator: isOperator
 };
